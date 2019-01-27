06c37f5f13b8a3582a6e22618382e428086408e9
David Forer <david.j.forer@gmail.com>
2019-01-27 18:01:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,10 @@
-def reverse_each_word(sentence)
-	sentence.split(' ').collect{ |w| w.reverse }.join(' ')
+def reverse_each_word(input)
+
+ temp = input.split(" ")
+temp2 = []
+temp.collect do |word|
+  temp2.push(word.reverse)
+end
+output = temp2.join(" ")
+return output
 end
